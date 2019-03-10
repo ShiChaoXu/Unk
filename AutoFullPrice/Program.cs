accec833c39acd14a83920ed18503b4354eed20a
xusc1 <xusc1@lenovo.com>
2019-03-10 19:25:10 +0800

    update price random percentage

--- AutoFullPrice/Program.cs
+++ AutoFullPrice/Program.cs
@@ -15,8 +15,9 @@ namespace AutoFullPrice
         static void Main(string[] args)
         {
             string action = args[0];
-
-
+            Random rd = new Random();
+            double r = rd.Next(101, 111);
+            r = r / 100;
             switch (action)
             {
                 case "AutoFullPriceHistory":
@@ -26,7 +27,7 @@ namespace AutoFullPrice
                     using (SqlConnection conn =new SqlConnection(SQLConnectionStr))
                     {
                         var vv_prices = conn.Query<decimal>($@"SELECT top(1) CurrentPrice FROM [GainsInHistory] WHERE TokenID = 'UNK' ORDER BY id DESC").FirstOrDefault();
-                        vv_prices = vv_prices * 1.1M;
+                        vv_prices = vv_prices * (decimal)r;
                         conn.Execute(GetSQl(vv_prices, DateTime.Now));
                     }
                     break;
